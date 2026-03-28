56260ec8127af7a31f8f7fe20f30db1742f57bd0
stephenyu <s.yu@less3.com>
2026-03-28 21:51:23 +1100

    Fixing Deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/open-clover/',
+  base: '/openover/',
   server: {
     allowedHosts: true,
   },
